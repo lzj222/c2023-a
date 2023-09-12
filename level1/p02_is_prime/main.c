69ccbfb6c22843f46dc56488648f3572949dccc8
liuzijian2023 <1584695425@qq.com>
2023-09-12 21:30:33 +0800

    已提交

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,22 @@
-#include <printf.h>
+#include <stdio.h>
+int isprime(int n)
+{
+    for (int i = 2; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
 
+
+    }
+    return 1;
+}
 int main() {
-    printf("hello world!\n");
+    int n;
+    scanf("%d",&n);
+    if (isprime(n)) {
+        printf("是质数");
+    } else printf("不是质数");
     return 0;
 }
